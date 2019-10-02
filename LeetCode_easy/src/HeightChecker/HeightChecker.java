07a25237bced0aa216735edecf1604d832ffd14c
someyou9292 <51939321+someyou9292@users.noreply.github.com>
2019-10-02 16:34:04 +0900

    HeightChecker 문제 풀이 LeetCode 1051번 문제

---
+++ LeetCode_easy/src/HeightChecker/HeightChecker.java
@@ -0,0 +1,55 @@
+package HeightChecker;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/*
+키의 순서가 줄어들지 않게 줄을 맞춰야됩니다. 이때 올바른 위치에 있지 않은 학생들의 수를 구하시오
+ (오름차순으로 정렬했을때 제 위치에 있지않는 학생들의 수)
+ Input: [1,1,4,2,1,3]
+Output: 3
+ 4, 1, 3 은 제 위치에 없다
+ 1 1 1 2 3 4  가 되야되는 것
+ * */
+
+class Solution {
+	public int heightChecker(int[] heights) {
+
+		int[] sortedHeights = new int[heights.length];
+		
+		for(int i=0; i<heights.length;i++) {
+			sortedHeights[i] = heights[i];
+		}
+		Arrays.sort(sortedHeights);
+		
+		System.out.println("정렬된 배열");
+		for(int i=0; i<sortedHeights.length;i++) {
+			System.out.print(sortedHeights[i] + " ");
+		}
+		System.out.println();
+		
+		System.out.println("원래의 배열");
+		for(int i=0; i<heights.length;i++) {
+			System.out.print(heights[i] + " ");
+		}
+		System.out.println();
+
+		int count = 0;
+		for (int i = 0; i < heights.length; i++) {
+			if(heights[i] != sortedHeights[i]) {
+				count++;
+			}
+		}
+		
+		return count;
+	}
+}
+
+public class HeightChecker {
+	public static void main(String[] args) {
+		Solution st = new Solution();
+		int[] heights = { 1, 2, 1, 2, 1, 1, 1, 2, 1 };
+
+		System.out.println(st.heightChecker(heights));
+	}
+}
