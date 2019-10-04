eb6ec62f10bb1f5d2cf52ffaf142267cec0746d8
someyou9292 <51939321+someyou9292@users.noreply.github.com>
2019-10-04 13:38:36 +0900

    10.04 알고리즘 풀이(1)

---
+++ LeetCode_easy/src/SortArrayByParity2/SortArrayByParity2.java
@@ -0,0 +1,48 @@
+package SortArrayByParity2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+ 	음수가 아닌 정수 배열이 주어지면
+ 	짝수 인덱스에는 짝수가, 홀수 인덱스에는 홀수가 배치되도록 정렬하시오
+ 	
+Example 1:
+
+Input: [4,2,5,7]
+Output: [4,5,2,7]
+Explanation: [4,7,2,5], [2,5,4,7], [2,7,4,5] would also have been accepted.
+ * */
+
+class Solution {
+	public int[] sortArrayByParityII(int[] A) {
+		int[] result = new int[A.length];
+		int index = 0;
+		int odd_index = 1;
+		int even_index = 0;
+
+		for (int i = 0; i < A.length; i++) {
+			if (A[i] % 2 == 0) {
+				result[even_index] = A[i];
+				even_index += 2;
+			} else {
+				result[odd_index] = A[i];
+				odd_index += 2;
+			}
+		}
+
+		for (int i = 0; i < result.length; i++) {
+			System.out.println(result[i]);
+		}
+		return result;
+	}
+}
+
+public class SortArrayByParity2 {
+	public static void main(String[] args) {
+		Solution st = new Solution();
+		int[] A = {4,2,5,7,1,8};
+		
+		st.sortArrayByParityII(A);
+	}
+}
